b6e7e8e8db253160f7e262d4500c2b6d2afe4779
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2022-04-30 17:01:23 +0200

    add image upload

--- public/js/add.js
+++ public/js/add.js
@@ -1,7 +1,7 @@
 import displayMessage from "./components/displayMessage.js";
 import createMenu from "./components/createMenu.js";
 import { getToken } from "./utils/storage.js";
-import { productsUrl } from "./settings/api.js";
+import { baseUrl, productsUrl } from "./settings/api.js";
 
 createMenu();
 
@@ -10,7 +10,7 @@ const message = document.querySelector(".message-container");
 const title = document.querySelector("#title");
 const price = document.querySelector("#price");
 const description = document.querySelector("#description");
-const file = document.querySelector("#formFile");
+const img = document.querySelector("#img");
 
 form.addEventListener("submit", formSubmit);
 
@@ -22,32 +22,45 @@ function formSubmit(event) {
   const titleValue = title.value.trim();
   const priceValue = price.value.trim();
   const descriptionValue = description.value.trim();
+  const imgValue = img.files[0];
 
   if (
     titleValue.length === 0 ||
     priceValue.length === 0 ||
-    descriptionValue === 0
+    descriptionValue === 0 ||
+    img.files.length === 0 ||
+    imgValue.size > 200000000 ||
+    (imgValue.type !== "image/jpeg" &&
+      imgValue.type !== "image/jpg" &&
+      imgValue.type !== "image/png")
   ) {
     return displayMessage(
       "alert-warning",
-      "Invalid value. There must be at least one character in each input.",
+      "Invalid value. There must be at least one character in each input and an image file.",
       ".message-container"
     );
   }
-  addProduct(titleValue, priceValue, descriptionValue, file);
+
+  addProduct(titleValue, priceValue, descriptionValue, imgValue);
 }
 
-async function addProduct(title, price, description, file) {
+async function addProduct(titleValue, priceValue, descriptionValue, imgValue) {
   const url = productsUrl;
   const data = JSON.stringify({
-    title: title,
-    price: price,
-    description: description,
-    file: file,
+    title: titleValue,
+    price: priceValue,
+    description: descriptionValue,
   });
-  console.log(data);
+
   const token = getToken();
 
+  //fetch product image
+
+  const formData = new FormData();
+  formData.append("files.image", imgValue, imgValue.name);
+  formData.append("data", JSON.stringify(data));
+
+  //
   const options = {
     method: "POST",
     body: data,
@@ -60,8 +73,10 @@ async function addProduct(title, price, description, file) {
   try {
     const response = await fetch(url, options);
     console.log(url);
+
     const json = await response.json();
     console.log(json);
+    console.log(data);
   } catch (error) {
     console.log(error);
   }

ed6c25ad0120f3589367b3c82768d3e9151ca8b6
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2022-05-15 11:51:45 +0300

    updated nav styles and layout

--- public/js/components/createMenu.js
+++ public/js/components/createMenu.js
@@ -1,41 +1,84 @@
 import { getUserName } from "../utils/storage.js";
 import logoutButton from "./logoutButton.js";
 
+// export default function createMenu() {
+//   const container = document.querySelector(".nav-container");
+//   const { pathname } = document.location;
+//   const username = getUserName();
+
+//   let authlink = `
+
+//         <a class="btn btn-primary btn-sm" id="login" href="/public/login.html" role="button">Login</a>
+//               `;
+//   if (username) {
+//     authlink = `
+//         <li class="nav-item dropdown">
+//           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+//             Admin
+//           </a>
+//           <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+//             <li><a class="dropdown-item ${
+//               pathname === "/public/add.html" ? "active" : ""
+//             }" href="/public/add.html">Add product</a></li>
+//             <li><a class="dropdown-item ${
+//               pathname === "/public/upload.html" ? "active" : ""
+//             }" href="/public/upload.html">Upload media</a></li>
+//             <li><a class="dropdown-item ${
+//               pathname === "/public/edit.html" ? "active" : ""
+//             }" href="/public/edit.html">Edit products</a></li>
+//           </ul>
+//         </li>
+//         <button type="button" class="btn btn-sm btn-outline-secondary px-4 gap-3" id="logout">
+//           Logout: ${username}
+//         </button>
+// `;
+//   }
+
+//   container.innerHTML = `
+//       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+//         <div class="container-fluid">
+//           <a class="navbar-brand" href="/public/index.html"><img src="/public/assets/soles-3-01.svg" alt="" style="width: 5rem;"></a>
+//           <button
+//             class="navbar-toggler"
+//             type="button"
+//             data-bs-toggle="collapse"
+//             data-bs-target="#navbarSupportedContent"
+//             aria-controls="navbarSupportedContent"
+//             aria-expanded="false"
+//             aria-label="Toggle navigation">
+//             <span class="navbar-toggler-icon"></span>
+//           </button>
+//           <div class="collapse navbar-collapse" id="navbarSupportedContent">
+//             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+//               <li class="nav-item">
+//                 <a class="nav-link ${
+//                   pathname === "/public/index.html" ? "active" : ""
+//                 }" aria-current="page" href="/public/index.html">Home</a>
+//               </li>
+//               <li class="nav-item">
+//                 <a class="nav-link ${
+//                   pathname === "/public/products.html" ? "active" : ""
+//                 }" href="/public/products.html">Products</a>
+//               </li>
+//               ${authlink}
+//             </ul>
+//             <a href="/public/cart.html">
+//               <i class="bi bi-bag-fill" style="font-size: 2rem;"></i>
+//             </a>
+//           </div>
+//         </div>
+//       </nav>`;
+
+//   logoutButton();
+// }
+
 export default function createMenu() {
   const container = document.querySelector(".nav-container");
   const { pathname } = document.location;
   const username = getUserName();
 
-  let authlink = `
-              
-       
-        <a class="btn btn-primary btn-sm" id="login" href="/public/login.html" role="button">Login</a>
-              `;
   if (username) {
-    authlink = `
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Admin
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item ${
-              pathname === "/public/add.html" ? "active" : ""
-            }" href="/public/add.html">Add product</a></li>
-            <li><a class="dropdown-item ${
-              pathname === "/public/upload.html" ? "active" : ""
-            }" href="/public/upload.html">Upload media</a></li>
-            <li><a class="dropdown-item ${
-              pathname === "/public/edit.html" ? "active" : ""
-            }" href="/public/edit.html">Edit products</a></li>
-          </ul>
-        </li>
-        <button type="button" class="btn btn-sm btn-outline-secondary px-4 gap-3" id="logout">
-          Logout: ${username}
-        </button>
-`;
-  }
-
-  container.innerHTML = `
+    container.innerHTML = `
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
           <a class="navbar-brand" href="/public/index.html"><img src="/public/assets/soles-3-01.svg" alt="" style="width: 5rem;"></a>
@@ -61,14 +104,69 @@ export default function createMenu() {
                   pathname === "/public/products.html" ? "active" : ""
                 }" href="/public/products.html">Products</a>
               </li>
-              ${authlink}
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Admin
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <li><a class="dropdown-item ${
+                    pathname === "/public/add.html" ? "active" : ""
+                  }" href="/public/add.html">Add product</a></li>
+                  <li><a class="dropdown-item ${
+                    pathname === "/public/upload.html" ? "active" : ""
+                  }" href="/public/upload.html">Upload media</a></li>
+                  <li><a class="dropdown-item ${
+                    pathname === "/public/edit.html" ? "active" : ""
+                  }" href="/public/edit.html">Edit products</a></li>
+                </ul>
+              </li>
             </ul>
+              <button type="button" class="btn btn-sm btn-outline-secondary px-4 gap-3" id="logout">
+                Logout: ${username}
+              </button>
             <a href="/public/cart.html">
               <i class="bi bi-bag-fill" style="font-size: 2rem;"></i>
             </a>
           </div>
         </div>
       </nav>`;
+  }
 
+  if (!username) {
+    container.innerHTML = `
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/public/index.html"><img src="/public/assets/soles-3-01.svg" alt="" style="width: 5rem;"></a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link ${
+                  pathname === "/public/index.html" ? "active" : ""
+                }" aria-current="page" href="/public/index.html">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link ${
+                  pathname === "/public/products.html" ? "active" : ""
+                }" href="/public/products.html">Products</a>
+              </li>
+            </ul>
+            <a class="nav-link" id="login" href="/public/login.html" role="button">Login</a>
+            <a href="/public/cart.html">
+              <i class="bi bi-bag-fill" style="font-size: 2rem;"></i>
+            </a>
+          </div>
+        </div>
+      </nav>`;
+  }
   logoutButton();
 }

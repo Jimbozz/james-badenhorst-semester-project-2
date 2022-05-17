fcc13f0749bdc26abcbc22ccafb7b501a0ba4fa6
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2022-05-17 12:35:47 +0200

    updated cart styles

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,6 +1,6 @@
 import { getProducts } from "./utils/storage.js";
+
 import { baseUrl } from "./settings/api.js";
-import deleteProduct from "./components/deleteProduct.js";
 import displayMessage from "./components/displayMessage.js";
 import createMenu from "./components/createMenu.js";
 
@@ -8,7 +8,7 @@ const products = getProducts();
 
 createMenu();
 
-const cartContainer = document.querySelector(".cart__items");
+const cartContainer = document.querySelector(".cart-items");
 // const cartTotal = document.querySelector(".cart__total");
 
 if (products.length === 0) {
@@ -17,7 +17,35 @@ if (products.length === 0) {
 
 products.forEach((product) => {
   cartContainer.innerHTML += `
-<div class="col">
+
+  <li class="list-group-item d-flex justify-content-between align-items-start ">
+    <div class="ms-2 me-auto">
+      <div class="fw-bold"><h5 class="card-title">${product.title}</h5></div>
+      <p class="card-text">$${product.price}</p>
+    </div>
+    <img src="${baseUrl}${product.image.url}" class="cart-items__image img-fluid" alt="${product.image.alternativeText}">
+    <a href="/public/product-specific.html?id=${product.id}" class="stretched-link"></a>
+  </li>
+  `;
+});
+
+/* total price */
+const totalContainer = document.querySelector(".total-container");
+
+let totalPrice = 0;
+
+for (let i = 0; i < products.length; i++) {
+  let price = parseFloat(products[i].price);
+
+  totalPrice += price;
+  console.log(products[i].price);
+}
+console.log(totalPrice);
+
+totalContainer.innerHTML += totalPrice;
+
+{
+  /* <div class="col">
   <div class="card mb-3" style="max-width: 540px;">
     <div class="row g-0">
       <div class="col-md-4">
@@ -33,23 +61,5 @@ products.forEach((product) => {
       </div>
     </div>
   </div>
-</div>
-
-  `;
-});
-
-deleteProduct();
-
-/* total price */
-const totalContainer = document.querySelector(".total-container");
-
-let totalPrice = 0;
-
-for (let i = 0; i < products.length; i++) {
-  let price = parseFloat(products[i].price);
-  totalPrice += price;
-  console.log(products[i].price);
+</div> */
 }
-console.log(totalPrice);
-
-totalContainer.innerHTML += totalPrice;
